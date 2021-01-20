06cb52d5b5555776629d5a81836023b76c4611f2
Jacopo Crocetta <jacopo.crocetta98@gmail.com>
2021-01-20 12:22:47 +0100

    Inserimento prima alberatura per inizio lavori

---
+++ src/main/java/com/project/BEbudgeting/BEbudgetingApplication.java
@@ -0,0 +1,13 @@
+package com.project.BEbudgeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BEbudgetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BEbudgetingApplication.class, args);
+	}
+
+}
