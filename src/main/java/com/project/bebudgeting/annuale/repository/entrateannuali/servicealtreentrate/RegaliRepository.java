759c283996dd1ec84ec740137ef723c700b43cc3
JacopoCrocetta <jacopo,crocetta98@gmail.com>
2021-07-14 22:28:48 +0200

    Repository

---
+++ src/main/java/com/project/bebudgeting/annuale/repository/entrateannuali/servicealtreentrate/RegaliRepository.java
@@ -0,0 +1,9 @@
+package com.project.bebudgeting.annuale.repository.entrateannuali.servicealtreentrate;
+
+import com.project.bebudgeting.annuale.entity.entrate.dettaglioaltreentrate.RegaliEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RegaliRepository extends CrudRepository<RegaliEntity, Integer> {
+}
