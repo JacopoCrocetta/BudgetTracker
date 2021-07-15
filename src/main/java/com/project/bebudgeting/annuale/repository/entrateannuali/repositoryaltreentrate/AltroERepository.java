798cf0213d2390453aa04188ce6ff25a3182629d
Jacopo Crocetta <jacopo.crocetta98@gmail.com>
2021-07-15 17:01:17 +0200

    Piccolo refactor
    Piccolo refactor per il Naming delle classi

--- src/main/java/com/project/bebudgeting/annuale/repository/entrateannuali/repositoryaltreentrate/AltroERepository.java
+++ src/main/java/com/project/bebudgeting/annuale/repository/entrateannuali/repositoryaltreentrate/AltroERepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AltroRepository extends CrudRepository<AltroEntity, Integer> {
+public interface AltroERepository extends CrudRepository<AltroEntity, Integer> {
 
 }
