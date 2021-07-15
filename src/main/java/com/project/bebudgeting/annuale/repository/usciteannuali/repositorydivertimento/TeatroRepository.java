378a2d0ddcf66f955ea3c62af83b2479cdfc5311
Jacopo Crocetta <jacopo.crocetta98@gmail.com>
2021-07-15 13:04:34 +0200

    Fixing Repository
    Fixing Repository

--- src/main/java/com/project/bebudgeting/annuale/repository/usciteannuali/repositorydivertimento/TeatroRepository.java
+++ src/main/java/com/project/bebudgeting/annuale/repository/usciteannuali/repositorydivertimento/TeatroRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TeatroRepository extends CrudRepository<TeatroEntity> {
+public interface TeatroRepository extends CrudRepository<TeatroEntity, Integer> {
 
 }
