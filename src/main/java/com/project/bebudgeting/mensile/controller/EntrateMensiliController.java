0deae21e6cce73f14eb54e3a0730abd48241f5bc
JacopoCrocetta <jacopo,crocetta98@gmail.com>
2021-08-29 00:35:32 +0200

    Prima stesura dei controller del BM

--- src/main/java/com/project/bebudgeting/mensile/controller/EntrateMensiliController.java
+++ src/main/java/com/project/bebudgeting/mensile/controller/EntrateMensiliController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,7 +29,21 @@ public class EntrateMensiliController {
     @Autowired
     EntratePrevisteService entratePrevisteService;
 
+    // COUNT
+    // Entrate Effettive
+    @GetMapping(value = "/getcountentrateeffettiveservice", produces = MediaType.APPLICATION_JSON_VALUE)
+    public long getCountEntrateEffettiveService() {
+        return entrateEffettiveService.count();
+    }
+
+    // Entrate Previste
+    @GetMapping(value = "/getcountentrateprevisteservice", produces = MediaType.APPLICATION_JSON_VALUE)
+    public long getCountEntratePrevisteService() {
+        return entratePrevisteService.count();
+    }
+
     // FIND
+    // Entrate Effettive
     @GetMapping(value = "/getallentrateeffettive", produces = MediaType.APPLICATION_JSON_VALUE)
     public Iterable<EntrateEffettiveEntity> findAllEntrateEffettive() {
         return entrateEffettiveService.findAll();
@@ -44,6 +59,7 @@ public class EntrateMensiliController {
         return entrateEffettiveService.findById(id);
     }
 
+    // Entrate Previste
     @GetMapping(value = "/getallentratepreviste", produces = MediaType.APPLICATION_JSON_VALUE)
     public Iterable<EntratePrevisteEntity> findAllEntratePreviste() {
         return entratePrevisteService.findAll();
@@ -60,32 +76,88 @@ public class EntrateMensiliController {
     }
 
     // DELETE
+    // Entrate Effettive
     @DeleteMapping(value = "/deleteallentrateeffettive")
     public void deleteAllEntarteEffettve() {
         entrateEffettiveService.deleteAll();
     }
 
     @DeleteMapping(value = "/deleteoneentrateeffettiveentity", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void deleteOneEntity(@RequestBody EntrateEffettiveEntity entity)
+    public void deleteOneEntrataEffettiva(@RequestBody EntrateEffettiveEntity entity)
             throws NotFoundException, NullPointerException, NotImplementedException {
         entrateEffettiveService.delete(entity);
     }
 
     @DeleteMapping(value = "/deletesomeeffettiveentityentity", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void deleteSomeEntity(@RequestBody Iterable<EntrateEffettiveEntity> entities)
+    public void deleteSomeEntrateEffettive(@RequestBody Iterable<EntrateEffettiveEntity> entities)
             throws NotFoundException, NullPointerException, NotImplementedException {
         entrateEffettiveService.deleteAll(entities);
     }
 
     @DeleteMapping(value = "/deleteeffettiveentityentitybyid")
-    public void getFilteredSumEstimatedRevenueByDate(@RequestParam int id)
+    public void deleteOneEntrataEffettivaById(@RequestParam int id)
             throws NotFoundException, NullPointerException, NotImplementedException {
         entrateEffettiveService.deleteById(id);
     }
 
-    @DeleteMapping(value = "/deletesomeeffettiveentity", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void deleteSomeEntityById(@RequestBody Iterable<Integer> entitiesId)
+    @DeleteMapping(value = "/deletesomeeffettiveentitybyids", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void deleteSomeEntrateEffettiveByIds(@RequestBody Iterable<Integer> entitiesId)
             throws NotFoundException, NullPointerException, NotImplementedException {
         entrateEffettiveService.deleteAllById(entitiesId);
+    }
+
+    // Entrate Previste
+    @DeleteMapping(value = "/deleteallentratepreviste")
+    public void deleteAllEntartePreviste() {
+        entratePrevisteService.deleteAll();
+    }
+
+    @DeleteMapping(value = "/deleteoneentrataprevistaentity", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void deleteOneEntrataPrevistaEntity(@RequestBody EntratePrevisteEntity entity)
+            throws NotFoundException, NullPointerException, NotImplementedException {
+        entratePrevisteService.delete(entity);
+    }
+
+    @DeleteMapping(value = "/deletesomeprevisteentity", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void deleteSomeEntity(@RequestBody Iterable<EntratePrevisteEntity> entities)
+            throws NotFoundException, NullPointerException, NotImplementedException {
+        entratePrevisteService.deleteAll(entities);
+    }
+
+    @DeleteMapping(value = "/deleteentrateprevisteentityentitybyid")
+    public void getFilteredSumEstimatedRevenueByDate(@RequestParam int id)
+            throws NotFoundException, NullPointerException, NotImplementedException {
+        entratePrevisteService.deleteById(id);
+    }
+
+    @DeleteMapping(value = "/deleteentrateprevisteentityentitybyids", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void deleteSomeEntityById(@RequestBody Iterable<Integer> entitiesId)
+            throws NotFoundException, NullPointerException, NotImplementedException {
+        entratePrevisteService.deleteAllById(entitiesId);
+    }
+
+    // SAVE
+    // Entrate Effettive
+    @PutMapping(value = "/saveoneentrataeffettiva", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public EntrateEffettiveEntity saveOneEntrataEffettivaEntity(@RequestBody EntrateEffettiveEntity entityToSave) {
+        return entrateEffettiveService.save(entityToSave);
+    }
+
+    @PutMapping(value = "/savesomeentrateeffettive", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<EntrateEffettiveEntity> saveSomeEntrateEffettiveEntities(
+            @RequestBody Iterable<EntrateEffettiveEntity> entitiesToSave) {
+        return entrateEffettiveService.saveAll(entitiesToSave);
+    }
+
+    // Entrate Previste
+    @PutMapping(value = "/saveoneentrataeffettiva", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public EntratePrevisteEntity saveOneEntrataPrevistaEntity(@RequestBody EntratePrevisteEntity entityToSave) {
+        return entratePrevisteService.save(entityToSave);
+    }
+
+    @PutMapping(value = "/savesomeentrateeffettive", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<EntratePrevisteEntity> saveSomeEntratePrevisteEntities(
+            @RequestBody Iterable<EntratePrevisteEntity> entitiesToSave) {
+        return entratePrevisteService.saveAll(entitiesToSave);
     }
 }
